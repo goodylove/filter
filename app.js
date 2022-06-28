464a980be2c95dc657b6a43f33938a8e16e93c3f
goodylove474 <goodyc474@gmail.com>
2022-06-28 07:47:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// ararry of objects
+
+const portfilo = [
+  { photo: "image/1.jpg", name: "building" },
+  { photo: "image/2.jpg", name: "building" },
+  { photo: "image/3.jpg", name: "building" },
+  { photo: "image/f1.jpg", name: "furniture" },
+  { photo: "image/f2.jpg", name: "furniture" },
+  { photo: "image/f3.jpg", name: "furniture" },
+  { photo: "image/ferari.jpeg", name: "cars" },
+  { photo: "image/ford.jpeg", name: "cars" },
+  { photo: "image/lambo.webp", name: "cars" },
+];
+
+const myPortFilo = portfilo.map((v) => {
+  return `<div class="img-cont">
+   <img src="${v.photo}" class="all-img" >
+ </div>`;
+});
+document.querySelector(".display").innerHTML = `${myPortFilo.join("")}`;
+let btn = [...document.querySelectorAll(".btn")];
+
+btn.forEach((v, a) => {
+  v.addEventListener("click", (e) => {
+    let current = document.getElementsByClassName("active");
+
+   
+    v.value = v.value.toLowerCase();
+    if (v.value == "show all") {
+      document.querySelector(".display").innerHTML = `${myPortFilo.join("")}`;
+    } else {
+      if (v.value == "cars" || "Furniture" || "building") {
+        current.className += v;
+        const allPortfilo = portfilo.filter((p) => {
+          return p.name == v.value;
+        });
+        const showAll = allPortfilo.map((v) => {
+          return `<div class="img-cont">
+      <img src="${v.photo}" class="all-img" >
+    </div>`;
+        });
+        document.querySelector(".display").innerHTML = `${showAll.join("")}`;
+      }
+    }
+  });
+});
+
+// {/* // Create an object: */}
+// var person = {
+//   firstName  : "John",
+//   lastName   : "Doe",
+//   id     : 5566,
+//   myFunction : function() {
+//     return `<div>
+//      ${this.firstName + " " + this.lastName}
+//     </div>`
+//   }
+
+// };
+
+//   document.getElementById("demo").innerHTML =`<div>
+//   ${ person.myFunction()}
+//   </div>`
